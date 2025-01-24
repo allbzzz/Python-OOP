664d829de94d3f0d577bf4eebc215098843337ef
allbzzz <146387808+allbzzz@users.noreply.github.com>
2025-01-24 21:14:15 +0300

    Add files via upload

---
+++ 6-2-6.py
@@ -0,0 +1,22 @@
+"""
+Создайте перечисление Size, в котором хранятся размеры одежды:
+
+S - small
+M - medium
+L - large
+XL - extra large
+XXL - extra extra large
+В списке сперва указаны названия атрибута, затем его строковое значение
+
+Ваша задача написать только определения класса Size
+"""
+
+
+from enum import Enum
+
+class Size(Enum):
+    S = 'small'
+    M = 'medium'
+    L = 'large'
+    XL = 'extra large'
+    XXL = 'extra extra large'
